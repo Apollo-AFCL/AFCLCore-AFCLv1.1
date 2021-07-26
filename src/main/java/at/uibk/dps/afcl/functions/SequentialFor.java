98e2741dbf9e810a72f1f9c713431fc6fccd2b31
Fedor <fedor@dps.uibk.ac.at>
2021-07-26 09:16:36 +0200

    changed the seqFor syntax. Removed seqWhile and switch

--- src/main/java/at/uibk/dps/afcl/functions/SequentialFor.java
+++ src/main/java/at/uibk/dps/afcl/functions/SequentialFor.java
@@ -1,10 +1,9 @@
 package at.uibk.dps.afcl.functions;
 
 import at.uibk.dps.afcl.Function;
+import at.uibk.dps.afcl.functions.objects.Condition;
 import at.uibk.dps.afcl.functions.objects.DataIns;
-import at.uibk.dps.afcl.functions.objects.DataLoops;
 import at.uibk.dps.afcl.functions.objects.DataOuts;
-import at.uibk.dps.afcl.functions.objects.LoopCounter;
 import com.fasterxml.jackson.annotation.*;
 
 import java.util.List;
@@ -16,86 +15,90 @@ import java.util.Objects;
  * @author stefanpedratscher
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "name", "dataIns", "dataLoops", "loopCounter", "loopBody", "dataOuts" })
+@JsonPropertyOrder({"name", "dataIns", "iterator", "loopBody", "condition"})
 @JsonTypeName("for")
 @JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
 public class SequentialFor extends LoopCompound {
 
-    /**
-     * Contains needed information about potential loop data flow
-     */
-    @JsonProperty("dataLoops")
-    private List<DataLoops> dataLoops;
+  /**
+   * Provides the information on the number of sequential executions of the bosy.
+   */
+  @JsonProperty("iterator")
+  protected String iterator;
 
-    /**
-     * Contains needed information about the number of (sequential) loop iterations
-     */
-    @JsonProperty("loopCounter")
-    private LoopCounter loopCounter;
+  /**
+   * Provides the stop condition(s)
+   */
+  @JsonProperty("condition")
+  protected List<Condition> condition;
 
-    /**
-     * Default constructor.
-     */
-    public SequentialFor() {
-        super();
+  /**
+   * Default constructor.
+   */
+  public SequentialFor() {
+    super();
+  }
+
+  /**
+   * Constructor for function compound for
+   *
+   * @param name Unique identifier of the compound
+   * @param dataIns Data input ports ({@link DataIns})
+   * @param iterator information about the number of (sequential) loop iterations
+   * @param loopBodySequentialFor functions which should be executed in each
+   *        iteration
+   * @param condition stop condition
+   * @param dataOuts Data output ports ({@link DataOuts})
+   */
+  public SequentialFor(final String name, final List<DataIns> dataIns, final String iterator,
+      final List<Function> loopBodySequentialFor, final List<Condition> condition,
+      final List<DataOuts> dataOuts) {
+    this();
+    this.name = name;
+    this.dataIns = dataIns;
+    this.setLoopBody(loopBodySequentialFor);
+    this.dataOuts = dataOuts;
+    this.iterator = iterator;
+    this.condition = condition;
+  }
+
+  @JsonProperty("iterator")
+  public String getIterator() {
+    return iterator;
+  }
+
+  @JsonProperty("iterator")
+  public void setIterator(String iterator) {
+    this.iterator = iterator;
+  }
+
+  @JsonProperty("condition")
+  public List<Condition> getCondition() {
+    return condition;
+  }
+
+  @JsonProperty("condition")
+  public void setCondition(List<Condition> condition) {
+    this.condition = condition;
+  }
+
+  @Override
+  public boolean equals(final Object object) {
+    if (this == object) {
+      return true;
     }
-
-    /**
-     * Constructor for function compound for
-     *
-     * @param name        Unique identifier of the compound
-     * @param dataIns     Data input ports ({@link DataIns})
-     * @param loopCounter information about the number of
-     *                    (sequential) loop iterations
-     * @param loopBodySequentialFor    functions which should be
-     *                                 executed in each iteration
-     * @param dataOuts    Data output ports ({@link DataOuts})
-     */
-    public SequentialFor(final String name, final List<DataIns> dataIns, final List<DataLoops> dataLoops, final LoopCounter loopCounter,
-                         final List<Function> loopBodySequentialFor, final List<DataOuts> dataOuts) {
-        this();
-        this.name = name;
-        this.dataIns = dataIns;
-        this.dataLoops = dataLoops;
-        this.loopCounter = loopCounter;
-        this.setLoopBody(loopBodySequentialFor);
-        this.dataOuts = dataOuts;
+    if (object == null || getClass() != object.getClass()) {
+      return false;
     }
-
-    @JsonProperty("dataLoops")
-    public List<DataLoops> getDataLoops() { return dataLoops; }
-
-    @JsonProperty("dataLoops")
-    public void setDataLoops(final List<DataLoops> dataLoops) { this.dataLoops = dataLoops; }
-
-    @JsonProperty("loopCounter")
-    public LoopCounter getLoopCounter() {
-        return loopCounter;
+    if (!super.equals(object)) {
+      return false;
     }
+    final SequentialFor that = (SequentialFor) object;
+    return Objects.equals(condition, that.condition) && Objects.equals(iterator, that.iterator);
+  }
 
-    @JsonProperty("loopCounter")
-    public void setLoopCounter(final LoopCounter loopCounterSequentialFor) {
-        this.loopCounter = loopCounterSequentialFor;
-    }
-
-    @Override
-    public boolean equals(final Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        if (!super.equals(object)) {
-            return false;
-        }
-        final SequentialFor that = (SequentialFor) object;
-        return Objects.equals(dataLoops, that.dataLoops) &&
-                Objects.equals(loopCounter, that.loopCounter);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), dataLoops, loopCounter);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), condition, iterator);
+  }
 }
--- src/test/java/at/uibk/dps/afcl/functions/SequentialForTest.java
+++ src/test/java/at/uibk/dps/afcl/functions/SequentialForTest.java
@@ -1,13 +1,12 @@
 package at.uibk.dps.afcl.functions;
 
+import at.uibk.dps.afcl.functions.objects.Condition;
 import at.uibk.dps.afcl.functions.objects.DataIns;
-import at.uibk.dps.afcl.functions.objects.DataLoops;
 import at.uibk.dps.afcl.functions.objects.DataOuts;
-import at.uibk.dps.afcl.functions.objects.LoopCounter;
 import org.junit.Assert;
 import org.junit.Test;
 import org.meanbean.test.BeanTester;
-
+import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -20,117 +19,107 @@ import java.util.List;
  */
 public class SequentialForTest {
 
-    /**
-     * Test full construction of a sequentialFor.
-     *
-     * @author stefanpedratscher
-     */
-    @Test
-    public void testFullConstruction() {
-        final AtomicFunction atomicFunction = new AtomicFunction("atomicFunction", "atomicFunctionType", null, null);
-        final DataIns dataIns = new DataIns("inName", "inType");
-        final DataOuts dataOuts = new DataOuts("outName", "outType", "outSource");
-        final DataLoops dataLoops = new DataLoops();
-        final LoopCounter loopCounter = new LoopCounter();
+  /**
+   * Test full construction of a sequentialFor.
+   *
+   * @author stefanpedratscher
+   */
+  @Test
+  public void testFullConstruction() {
+    final AtomicFunction atomicFunction =
+        new AtomicFunction("atomicFunction", "atomicFunctionType", null, null);
+    final DataIns dataIns = new DataIns("inName", "inType");
+    final DataOuts dataOuts = new DataOuts("outName", "outType", "outSource");
+    final String iterator = "5";
+    final List<Condition> condition =
+        Arrays.asList(new Condition("1", "2", "number", "==", "AND", false),
+            new Condition("1", "1", "number", "==", "AND", false));
 
-        final SequentialFor sequentialFor = new SequentialFor("sequentialFor",
-                new ArrayList<>(Collections.singleton(dataIns)),
-                new ArrayList<>(Collections.singleton(dataLoops)),
-                loopCounter,
-                new ArrayList<>(Collections.singleton(atomicFunction)),
-                new ArrayList<>(Collections.singleton(dataOuts)));
+    final SequentialFor sequentialFor =
+        new SequentialFor("sequentialFor", new ArrayList<>(Collections.singleton(dataIns)),
+            iterator, new ArrayList<>(Collections.singleton(atomicFunction)), condition,
+            new ArrayList<>(Collections.singleton(dataOuts)));
 
-        Assert.assertEquals("sequentialFor", sequentialFor.getName());
+    Assert.assertEquals("sequentialFor", sequentialFor.getName());
 
-        Assert.assertEquals(1, sequentialFor.getDataIns().size());
-        Assert.assertEquals(dataIns, sequentialFor.getDataIns().get(0));
-        Assert.assertEquals(dataIns.hashCode(), sequentialFor.getDataIns().get(0).hashCode());
+    Assert.assertEquals(1, sequentialFor.getDataIns().size());
+    Assert.assertEquals(dataIns, sequentialFor.getDataIns().get(0));
+    Assert.assertEquals(dataIns.hashCode(), sequentialFor.getDataIns().get(0).hashCode());
 
-        Assert.assertEquals(1, sequentialFor.getDataLoops().size());
-        Assert.assertEquals(dataLoops, sequentialFor.getDataLoops().get(0));
-        Assert.assertEquals(dataLoops.hashCode(), sequentialFor.getDataLoops().get(0).hashCode());
+    assertEquals(iterator, sequentialFor.getIterator());
+    assertEquals(condition, sequentialFor.getCondition());
 
-        Assert.assertEquals(loopCounter, sequentialFor.getLoopCounter());
-        Assert.assertEquals(loopCounter.hashCode(), sequentialFor.getLoopCounter().hashCode());
+    Assert.assertEquals(1, sequentialFor.getLoopBody().size());
+    Assert.assertEquals(atomicFunction, sequentialFor.getLoopBody().get(0));
+    Assert.assertEquals(atomicFunction.hashCode(), sequentialFor.getLoopBody().get(0).hashCode());
 
-        Assert.assertEquals(1, sequentialFor.getLoopBody().size());
-        Assert.assertEquals(atomicFunction, sequentialFor.getLoopBody().get(0));
-        Assert.assertEquals(atomicFunction.hashCode(), sequentialFor.getLoopBody().get(0).hashCode());
+    Assert.assertEquals(1, sequentialFor.getDataOuts().size());
+    Assert.assertEquals(dataOuts, sequentialFor.getDataOuts().get(0));
+    Assert.assertEquals(dataOuts.hashCode(), sequentialFor.getDataOuts().get(0).hashCode());
 
-        Assert.assertEquals(1, sequentialFor.getDataOuts().size());
-        Assert.assertEquals(dataOuts, sequentialFor.getDataOuts().get(0));
-        Assert.assertEquals(dataOuts.hashCode(), sequentialFor.getDataOuts().get(0).hashCode());
+    Assert.assertEquals(0, atomicFunction.getAdditionalProperties().size());
+  }
 
-        Assert.assertEquals(0, atomicFunction.getAdditionalProperties().size());
-    }
+  /**
+   * Test the empty construction of a sequentialFor.
+   *
+   * @author stefanpedratscher
+   */
+  @Test
+  public void testEmptyConstruction() {
+    final SequentialFor sequentialFor = new SequentialFor();
 
-    /**
-     * Test the empty construction of a sequentialFor.
-     *
-     * @author stefanpedratscher
-     */
-    @Test
-    public void testEmptyConstruction() {
-        final SequentialFor sequentialFor = new SequentialFor();
+    Assert.assertNull(sequentialFor.getName());
+    Assert.assertNull(sequentialFor.getDataIns());
+    Assert.assertNull(sequentialFor.getIterator());
+    Assert.assertNull(sequentialFor.getCondition());
+    Assert.assertNull(sequentialFor.getLoopBody());
+    Assert.assertNull(sequentialFor.getDataOuts());
+    Assert.assertEquals(0, sequentialFor.getAdditionalProperties().size());
+  }
 
-        Assert.assertNull(sequentialFor.getName());
-        Assert.assertNull(sequentialFor.getDataIns());
-        Assert.assertNull(sequentialFor.getDataLoops());
-        Assert.assertNull(sequentialFor.getLoopCounter());
-        Assert.assertNull(sequentialFor.getLoopBody());
-        Assert.assertNull(sequentialFor.getDataOuts());
-        Assert.assertEquals(0, sequentialFor.getAdditionalProperties().size());
-    }
+  /**
+   * Test getter and setter
+   *
+   * @author stefanpedratscher
+   */
+  @Test
+  public void testGetterAndSetter() {
+    new BeanTester().testBean(SequentialFor.class);
+  }
 
-    /**
-     * Test getter and setter
-     *
-     * @author stefanpedratscher
-     */
-    @Test
-    public void testGetterAndSetter() {
-        new BeanTester().testBean(SequentialFor.class);
-    }
+  /**
+   * Test hashCode and equals
+   *
+   * @author stefanpedratscher
+   */
+  @Test
+  public void testHashEquals() {
+    final SequentialFor sequentialFor = new SequentialFor("name", null, null, null, null, null);
 
-    /**
-     * Test hashCode and equals
-     *
-     * @author stefanpedratscher
-     */
-    @Test
-    public void testHashEquals() {
-        final SequentialFor sequentialFor = new SequentialFor("name", null, null, null, null, null);
+    Assert.assertEquals(sequentialFor, sequentialFor);
+    Assert.assertEquals(sequentialFor.hashCode(), sequentialFor.hashCode());
+    Assert.assertNotEquals(sequentialFor, null);
 
-        Assert.assertEquals(sequentialFor, sequentialFor);
-        Assert.assertEquals(sequentialFor.hashCode(), sequentialFor.hashCode());
-        Assert.assertNotEquals(sequentialFor, null);
+    final LoopCompound loopCompound = new LoopCompound();
+    Assert.assertNotEquals(sequentialFor, loopCompound);
 
-        final LoopCompound loopCompound = new LoopCompound();
-        Assert.assertNotEquals(sequentialFor, loopCompound);
+    final SequentialFor sequentialFor2 = new SequentialFor("name", null, null, null, null, null);
+    Assert.assertEquals(sequentialFor, sequentialFor2);
+    Assert.assertEquals(sequentialFor.hashCode(), sequentialFor2.hashCode());
+    sequentialFor2.setAdditionalProperties("name", "type");
+    Assert.assertNotEquals(sequentialFor, sequentialFor2);
 
-        final SequentialFor sequentialFor2 = new SequentialFor("name", null, null, null, null, null);
-        Assert.assertEquals(sequentialFor, sequentialFor2);
-        Assert.assertEquals(sequentialFor.hashCode(), sequentialFor2.hashCode());
-        sequentialFor2.setAdditionalProperties("name", "type");
-        Assert.assertNotEquals(sequentialFor, sequentialFor2);
+    SequentialFor sequentialFor3;
+    sequentialFor3 = new SequentialFor("nameWrong", null, null, null, null, null);
+    Assert.assertNotEquals(sequentialFor, sequentialFor3);
 
-        SequentialFor sequentialFor3;
-        sequentialFor3 = new SequentialFor("nameWrong", null, null, null, null, null);
-        Assert.assertNotEquals(sequentialFor, sequentialFor3);
+    sequentialFor3 = new SequentialFor("name",
+        Collections.singletonList(new DataIns("name", "type", "source")), null, null, null, null);
+    Assert.assertNotEquals(sequentialFor, sequentialFor3);
 
-        sequentialFor3 = new SequentialFor("name", Collections.singletonList(new DataIns("name", "type", "source")), null, null, null, null);
-        Assert.assertNotEquals(sequentialFor, sequentialFor3);
-
-        sequentialFor3 = new SequentialFor("name", null, Collections.singletonList(new DataLoops()), null, null, null);
-        Assert.assertNotEquals(sequentialFor, sequentialFor3);
-
-        sequentialFor3 = new SequentialFor("name", null, null, new LoopCounter(), null, null);
-        Assert.assertNotEquals(sequentialFor, sequentialFor3);
-
-        sequentialFor3 = new SequentialFor("name", null, null, null, Collections.singletonList(new AtomicFunction()), null);
-        Assert.assertNotEquals(sequentialFor, sequentialFor3);
-
-        sequentialFor3 = new SequentialFor("name", null, null, null, null, Collections.singletonList(new DataOuts("name", "type", "source")));
-        Assert.assertNotEquals(sequentialFor, sequentialFor3);
-    }
+    sequentialFor3 = new SequentialFor("name", null, null, null, null,
+        Collections.singletonList(new DataOuts("name", "type", "source")));
+    Assert.assertNotEquals(sequentialFor, sequentialFor3);
+  }
 }
